43f059b679e95d327770f59a1956e37e874d39a0
N3mxara <nimsara.20223294@iit.ac.lk>
2023-06-05 12:29:57 +0530

    fuck

---
+++ src/main2.java
@@ -0,0 +1,46 @@
+import java.util.Scanner;
+
+public class main2 {
+
+    public void foodoptions(){
+        System.out.println ("100 or VFQ: View all Queues.");
+        System.out.println ("101 or VEQ: View all Empty Queues.");
+        System.out.println ("102 or ACQ: Add customer to a Queue.");
+        System.out.println("103 or RCQ: Remove a customer from a Queue. (From a specific location)");
+        System.out.println("104 or PCQ: Remove a served customer.");
+        System.out.println("105 or VCS: View Customers Sorted in alphabetical order (Do not use library sort routine)");
+        System.out.println("106 or SPD: Store Program Data into file.");
+        System.out.println("107 or LPD: Load Program Data from file.");
+        System.out.println("108 or STK: View Remaining burgers Stock.");
+        System.out.println("109 or AFS: Add burgers to Stock.");
+        System.out.println("999 or EXT: Exit the Program.\n");
+        return ;
+    }
+
+    private static final int MaxCashierCustomers1 = 2;
+    private static final int MaxCashierCustomers2 = 3;
+    private static final int MaxCashierCustomers3 = 5;
+    public static void main(String[] args) {
+
+        System.out.println(System.lineSeparator()+"Methods Of the foody fav ");
+        System.out.println("===========*****============" + System.lineSeparator());
+
+        //calling to menu method from main method
+        main2 obj = new main2();
+        obj.foodoptions();
+
+        //Calling a input to continue
+        Scanner route = new Scanner(System.in);
+        System.out.print("Press the method number to continue : ");
+        String inputMenu = route.nextLine();
+
+        switch (inputMenu){
+            case ()
+        }
+
+
+
+
+
+    }
+}
