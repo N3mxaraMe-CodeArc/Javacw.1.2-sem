a98a696f1b77b75e18992493b9eb27fdc69d7909
N3mxara <nimsara.20223294@iit.ac.lk>
2023-06-05 13:37:04 +0530

    new

--- src/main2.java
+++ src/main2.java
@@ -2,6 +2,15 @@ import java.util.Scanner;
 
 public class main2 {
 
+    private void viewAllQueues(){
+        System.out.println("Show All quoues");
+    }
+    private void viewEmptyQueues(){
+        System.out.println("Empty Quoues");
+    }
+    private void addCustomerToQueues(){
+        System.out.println("Add customer to quoues");
+    }
     public void foodoptions(){
         System.out.println ("100 or VFQ: View all Queues.");
         System.out.println ("101 or VEQ: View all Empty Queues.");
@@ -13,7 +22,7 @@ public class main2 {
         System.out.println("107 or LPD: Load Program Data from file.");
         System.out.println("108 or STK: View Remaining burgers Stock.");
         System.out.println("109 or AFS: Add burgers to Stock.");
-        System.out.println("999 or EXT: Exit the Program.\n");
+        System.out.println("999 or EXT: Exit the Program.\n");
         return ;
     }
 
@@ -34,9 +43,21 @@ public class main2 {
         System.out.print("Press the method number to continue : ");
         String inputMenu = route.nextLine();
 
-        if(inputMenu == "100" || inputMenu =="VFQ"){
+        //import main method objects
+        main2 emptyquoues = new main2();
 
+        //Select quoue method
+
+        if (inputMenu.equals("100") || inputMenu.equals("VFQ")){
+            emptyquoues.viewAllQueues();
         }
+        else if(inputMenu.equals("101") || inputMenu.equals("VEQ")){
+            emptyquoues.viewEmptyQueues();
+        }
+        else if(inputMenu.equals("102") || inputMenu.equals("ACQ")){
+            emptyquoues.addCustomerToQueues();
+        }
+
 
 
 
