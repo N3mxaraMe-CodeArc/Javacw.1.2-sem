981c6df55d142b91cb0e272370fa238fd3c152f9
N3mxara <nimsara.20223294@iit.ac.lk>
2023-06-08 14:48:36 +0530

    new

--- src/main2.java
+++ src/main2.java
@@ -1,5 +1,4 @@
 import java.util.Scanner;
-import java.util.Stack;
 
 public class main2 {
 
@@ -24,7 +23,7 @@ public class main2 {
         System.out.println("Empty Quoues");
     }
     private void addCustomerToQueues(){
-        System.out.println("Add customer to quoues");
+
     }
     private void RemCustomerToQueues(){
         System.out.println("Remove customer to quoues");
@@ -50,14 +49,129 @@ public class main2 {
     private void Exit(){
         System.out.println("Load Program Data from file");
     }
+    private void ViewIncome(){
+        System.out.println("Total Income : "+ (burgerCount*650));
+    }
+
+    //----------------------  Name / burger Validation Part  -------------------------
+    String customerName = "";
+    public String nameValid(String nameType) {
+        while (true) {
+            Scanner route = new Scanner(System.in);
+            System.out.print("Enter Your " + nameType + ": ");
+            String name = route.nextLine();
+            if (!name.isEmpty() && !name.trim().isEmpty()) {
+                try {
+                    Integer.parseInt(name);
+                    System.out.println("Enter a valid name");
+                } catch (NumberFormatException e) {
+                    customerName = name.toUpperCase();
+                    break;
+                }
+            } else {
+                System.out.println("Enter a valid name");
+            }
+        }
+        return customerName;
+    }
+
+
+     int cashierCustomers1 = 0;
+     int cashierCustomers2 = 0;
+    int cashierCustomers3 = 0;
+    int totalCustomers = 0;
+    int burgerCount = 0 ;
+    static final int MaxCashierCustomers1 = 2;
+    static final int MaxCashierCustomers2 = 3;
+    static final int MaxCashierCustomers3 = 5;
+    static final int maxCustomers=10;
+
+
+
+    //----------------- Class burger count and Validation -------------------------------------
+
+    public int burgerCount(String NoOfBurgers){
+
+        System.out.println("No of " + NoOfBurgers +" : ");
+        Scanner route = new Scanner(System.in);
+        int BurgerRequaired = route.nextInt();
+        burgerCount = (BurgerRequaired+burgerCount);
+
+        return BurgerRequaired;
+    }
+
 
     static class customers{
 
+        main2 validateName = new main2();
+        String firstName = validateName.nameValid("First Name");
+        String lastName = validateName.nameValid("Last Name");
+
+        main2 objBurger = new main2();
+        int RequairedBurger = objBurger.burgerCount("Burgers Requaired");
+
     }
 
-    static class foodQueue{
+    //------------------------------ Food Queue Part ------------------------------------
 
+
+
+    class foodQueue{
+
+        Scanner route = new Scanner(System.in);
+
+        while (true) {
+            try {
+                if (totalCustomers == maxCustomers) {
+                    System.out.println("All cashiers are full. Program exiting...");
+                }
+
+                System.out.print("Enter the cashier number (1, 2, 3): ");
+                int cashierNumber = route.nextInt();
+
+                switch (cashierNumber) {
+                    case 1:
+                        if (cashierCustomers1 < MaxCashierCustomers1) {
+                            cashierCustomers1++;
+                            totalCustomers++;
+                            System.out.println("Customer added to cashier 1");
+                        } else {
+                            System.out.println("Cashier 1 is full. Please choose another cashier.");
+                        }
+                        break;
+
+                    case 2:
+                        if (cashierCustomers2 < MaxCashierCustomers2) {
+                            cashierCustomers2++;
+                            totalCustomers++;
+                            System.out.println("Customer added to cashier 2");
+                        } else {
+                            System.out.println("Cashier 2 is full. Please choose another cashier.");
+                        }
+                        break;
+
+                    case 3:
+                        if (cashierCustomers3 < MaxCashierCustomers3) {
+                            cashierCustomers3++;
+                            totalCustomers++;
+                            System.out.println("Customer added to cashier 3");
+                        } else {
+                            System.out.println("Cashier 3 is full. Please choose another cashier.");
+                        }
+                        break;
+
+                    default:
+                        System.out.println("Invalid cashier number. Please try again.");
+                        break;
+                }
+            } catch (Exception e) {
+                System.out.println("Invalid input. Please try again.");
+                route.nextLine();
+            }
+        }
     }
+
+    //-------------------------- Food Option Part ------------------------------------------
 
     public void foodoptions(){
         System.out.println ("100 or VFQ: View all Queues.");
@@ -70,13 +184,16 @@ public class main2 {
         System.out.println("107 or LPD: Load Program Data from file.");
         System.out.println("108 or STK: View Remaining burgers Stock.");
         System.out.println("109 or AFS: Add burgers to Stock.");
+        System.out.println("110 or ACQ: View income of each Queue.");
         System.out.println("999 or EXT: Exit the Program.");
         return ;
     }
 
-    private static final int MaxCashierCustomers1 = 2;
-    private static final int MaxCashierCustomers2 = 3;
-    private static final int MaxCashierCustomers3 = 5;
+    //-------------------------------------------------------------------------------------------------------------------------
+    //-------------------------------------------------------------------------------------------------------------------------
+
+    //----------------------- ------------------Main Method -------------------------------------------------------------------
+
     public static void main(String[] args) {
 
         System.out.println(System.lineSeparator()+"Methods Of the foody fav ");
@@ -101,7 +218,7 @@ public class main2 {
             obj.viewEmptyQueues();
         }
         else if(inputMenu.equals("102") || inputMenu.equals("ACQ")){
-            obj.addCustomerToQueues();
+            main2.customers customer = new main2.customers();
         }
         else if(inputMenu.equals("103") || inputMenu.equals("RCQ")){
             obj.RemCustomerToQueues();
@@ -123,8 +240,9 @@ public class main2 {
         }
         else if(inputMenu.equals("109") || inputMenu.equals("AFS")){
             obj.addBurgerToStock();
-        }
-        else if(inputMenu.equals("999") || inputMenu.equals("EXT")){
+        } else if (inputMenu.equals("110") || inputMenu.equals("IFQ")) {
+            obj.ViewIncome();
+        } else if(inputMenu.equals("999") || inputMenu.equals("EXT")){
             obj.Exit();
         }
     }
